96c6d51368beaf8e150bbe3017bad34dffdfc313
espadv69 <aitorjob69@gmail.com>
2025-01-25 13:26:59 +0100

    Integrate ColorPicker and ColorDisplay components into App component for color selection functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ import ColorPicker from './components/ColorPicker'
 import ColorDisplay from './components/ColorDisplay'
 
 const App = () => {
-  return <h1 className="h1-title">ColorPicker with React</h1>
+  const [selectedColor, setSelectedColor] = useState('nothing')
+
+  return (
+    <main>
+      <h1 className="h1-title">ColorPicker with React</h1>
+      <ColorPicker onColorSelect={(color) => setSelectedColor(color)} />
+      <ColorDisplay color={selectedColor} />
+    </main>
+  )
 }
 
 export default App
