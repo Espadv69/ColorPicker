1a6a5ff443117723c60c495885342e21be439f6c
espadv69 <aitorjob69@gmail.com>
2025-01-25 13:18:01 +0100

    Import ColorPicker and ColorDisplay components in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 
+import ColorPicker from './components/ColorPicker'
+import ColorDisplay from './components/ColorDisplay'
+
 const App = () => {
-  return (
-    <h1 className="h1-title">ColorPicker with React</h1>
-  )
+  return <h1 className="h1-title">ColorPicker with React</h1>
 }
 
 export default App
